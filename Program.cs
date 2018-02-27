9def73b4dc8ba1ab1352409e07277b07aace9254
Michalb96 <bugajski.michal96@gmail.com>
2018-02-27 13:39:33 +0100

    Let's di it

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Automaty_Komórkowe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
